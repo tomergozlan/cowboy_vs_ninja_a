f7492fa329f85ba6d7fbbbf253a55ec62d41ff50
Tomer Gozlan <tomgoz96@gmail.com>
2023-05-17 12:37:22 +0300

    Part 4 : SmartTeam.cpp && SmartTeam.hpp

---
+++ sources/SmartTeam.hpp
@@ -0,0 +1,43 @@
+//
+// Created by Tomer Gozlan on 17/05/2023.
+//
+
+#ifndef COWBOY_VS_NINJA_A_SMARTTEAM_HPP
+#define COWBOY_VS_NINJA_A_SMARTTEAM_HPP
+
+#include "Character.hpp"
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+namespace ariel {
+
+    class Team {
+    private:
+        Character *leader;
+        std::vector<Character *> fighters;
+    public:
+        Team(Character *leader);
+
+        Character* getLeader() const;
+
+        void setLeader(Character *newLeader);
+
+        const vector<Character*>& getFighters() const;
+
+        ~Team();
+
+        void add(Character *fighter);
+
+        Character* findClosestCharacter(const Point& location, const std::vector<Character*>& fighters) const;
+
+        void attack(Team *enemyTeam);
+
+        int stillAlive() const;
+
+        void print() const;
+
+    };
+
+}
+#endif //COWBOY_VS_NINJA_A_SMARTTEAM_HPP
--- sources/team2.cpp
+++
@@ -1,8 +0,0 @@
-//
-// Created by Tomer Gozlan on 17/05/2023.
-//
-
-#include "team2.hpp"
-
-namespace ariel {
-} // ariel
